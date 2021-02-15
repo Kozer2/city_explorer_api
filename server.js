0cc3eccdf0fa385c88723017bcfe211ee031c6f6
Ben Hemann <benhemann@yahoo.com>
2021-02-15 14:23:24 -0600

    done more stuff

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 'use strict'
 console.log('Server.js is connected');
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3111;
+console.log(`The port is: ${PORT}`);
+
+
+
+
+
+
+
+function Location(city, displayName, lat, lon) {
+    this.city = city;
+    this.displayName = displayName;
+    this.lat = parseFloat(lat);
+    this.lon = parseFloat(lon);
+}
