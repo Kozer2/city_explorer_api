58c3c61fc4bfca7e0f31e45f99416b760aba022e
Ben Hemann <benhemann@yahoo.com>
2021-02-16 19:00:44 -0600

    changed constructors, updated env

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ console.log('Server.js is connected');
 require('dotenv').config(); // required for dot env file
 
 const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
 
 
 const PORT = process.env.PORT || 3115;
 console.log(`The port is: ${PORT}`);
-const cors = require('cors');
 const app = express();
 
 // app.use(express.static('./public'));
@@ -18,60 +19,65 @@ app.get('/', (request, response)=> {
 
 // location path
 app.get('/location', (request, response) => { 
-    let search_query = request.query.city;
-    let locationData = require('./data/location.json');
-    let returnObject = {
-        search_query: search_query,
-        formatted_query: locationData[0].display_name,
-        latitude: locationData[0].lat,
-        longitude: locationData[0].lon
-    }
-    response.status(200).send(returnObject);
-    // const theDataObjFromJson = theDataArrayFromTheLocationJson[0];
-    // // console.log(request.query, response.query);
-    // const searchedCity = request.query.city;
-    // let newLocation = new Location(searchedCity,
-    //     theDataObjFromJson);
-    // response.send(newLocation);
+    let city = request.query.city;
+    let locationKey = process.env.LOCATION_KEY;
+    let url = `https://us1.locationiq.com/v1/search.php?key=${locationKey}&q=${city}&format=json`
+    superagent.get(url)
+        .then(saResults => {
+            let responseObj = new Location(saResults.body[0]);
+            response.status(200).send(responseObj);
+        })
     
-}); //.catch(error => {
-    //response.status(500).send('Location lookup failed')
-//});
+        .catch((error) => {
+            response.status(500).send('Location lookup failed', error);
+        })
+});
 
 // weather path
 app.get('/weather', (request, response) => { 
-    const theDataArrayFromTheWeatherJson = require('./data/weather.json');
-    const theDataObjFromJson = theDataArrayFromTheWeatherJson.data[0];
-    // console.log(request.query, response.query);
-    // const searchedCity = request.query.city;
-
-    const newWeather = new Weather( 
-        theDataObjFromJson.weather.description, 
-        theDataObjFromJson.valid_date);
-
-    response.send(newWeather);
-}); //.catch(error => {
-    //response.status(500).send('Weather lookup failed')
-//});
+    const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+    // console.log("request query", request.query.latitude);
+    const lat = parseFloat(request.query.latitude);
+    const lon = parseFloat(request.query.longitude);
+    let weatherArr = [];
+    let city = request.query.search_query;
+    const queryParams = {
+        key: process.env.WEATHER_KEY,
+        city: city,
+        lang: 'en',
+        days: 5
+    };
+    // console.log("request query", lat, lon);
+    return superagent.get(url)
+        .query(queryParams)
+        .then(weatherInfo => {
+            weatherInfo.body.data.forEach(day => {
+                weatherArr.push(new Weather(day));
+            })
+            console.log("Daily weather", weatherArr);
+            response.status(200).send(weatherArr);
+        });
+    // console.log(superagent);
+}); 
  
-app.use('*', (request,response)=> response.send("Route does not exist.")); // Needs to come after all other app gets 
+
 
 
 // location constructor
 function Location(object) {
-    this.searchQuery = object.searchQuery;
+    this.search_query = object.searchQuery;
     this.formattedQuery = object.display_name;
     this.latitude = object.lat;
     this.longitude = object.lon;
 }
 
 // weather constructor
-function Weather(weather, valid_date) {
-    this.weather = weather;
-    this.valid_date = valid_date;
+function Weather(day) {
+    this.forecast = day.weather.description;
+    this.time = day.datetime;
 }
 // data.weather.description
 
 // function to catch errors
-
+app.use('*', (request,response)=> response.send("Route does not exist.")); // Needs to come after all other app gets 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`)); // needs to be below the app.use('*')
